2533841bee250a1d0afd2489de3cdd9419b1e844
Nabiev-222 <81457667+Nabiev-222@users.noreply.github.com>
2022-11-30 19:23:59 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<title> Набиев Руслан ПИ-422</title>
+
+
+<table border="0" cellspacing="10">
+
+   <tr>
+     <th align="left">Лабораторная работа №1:</th>
+     <th align="left">Лабораторная работа №2:</th>
+	 <th align="left">Лабораторная работа №3:</th>
+     <th align="left">Лабораторная работа №4:</th>
+     <th align="left">Лабораторная работа №5:</th>
+   </tr>
+
+<tr>
+<td>
+
+<ul>
+  <li><a href="lab1/Lab1.html">Задание 1</a></li>
+  <li><a href="lab1/Lab2.html">Задание 2</a></li>
+  <li><a href="lab1/Lab3.html">Задание 3</a></li>
+  <li><a href="lab1/Lab4.html">Задание 4</a></li>
+  <li><a href="lab1/Lab5.html">Задание 5</a></li>
+  <li><a href="lab1/Lab6.html">Задание 6</a></li>
+  <li><a href="lab1/Lab7.html">Задание 7</a></li>
+</ul>
+</td>
+
+<td>
+<ul>
+  <li><a href="lab2/Lab1.html">Задание 1</a></li>
+  <li><a href="lab2/Lab2.html">Задание 2</a></li>
+  <li><a href="lab2/Lab3.html">Задание 3</a></li>
+  <li><a href="lab2/Lab4.html">Задание 4</a></li>
+  <li><a href="lab2/Lab5.html">Задание 5</a></li>
+  <li><a href="lab2/Lab6.html">Задание 6</a></li>
+  <li><a href="lab2/Lab7.html">Задание 7</a></li>
+</ul>
+</td>
+
+
+<td>
+
+<ul>
+  <li><a href="lab3/index.html">Запустить</a></li>
+</ul>
+</td>
+
+<td>
+
+<ul>
+  <li><a href="lab4/Lab4.html">Запустить</a></li>
+</ul>
+</td>
+<td>
+<ul>
+  <li><a href="lab5/index.html">Запустить</a></li>
+</ul>
+</td>
+</tr>
+
+
+</table>
